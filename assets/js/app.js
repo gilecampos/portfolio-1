93697091d38e40c4113d7f5fcc312382b2eeea7a
gileandrocampos <gileandrocampos8@gmail.com>
2024-02-18 09:40:54 -0300

    refactor: change function menuToggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,17 @@
-const btnNavMobile = document.querySelector("[data-js='btn-hamburger']")
+const mobileMenuButton = document.querySelector("[data-js='btn-hamburger']")
+const navbar = document.querySelector(".menu")
 
 const toggleNavbar = (event) => {
-  if(event.type === 'touchstart') {
+  if (event.type === 'touchstart') {
     event.preventDefault()
   }
 
-  const navbar = document.querySelector(".menu")
-  navbar.classList.toggle("active")
-  const navbarActive = navbar.classList.contains('active')
+  const navbarActive = navbar.classList.toggle("active")
   const ariaLabelTag = navbarActive ? "Fechar menu" : "Abrir menu"
-  
 
   event.currentTarget.setAttribute('aria-expanded', navbarActive)
   event.currentTarget.setAttribute('aria-label', ariaLabelTag)
 }
 
-btnNavMobile.addEventListener("click", toggleNavbar)
-btnNavMobile.addEventListener("touchstart", toggleNavbar)
+mobileMenuButton.addEventListener("click", toggleNavbar)
+mobileMenuButton.addEventListener("touchstart", toggleNavbar)
